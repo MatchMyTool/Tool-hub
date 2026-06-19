49a5679f9a82ba73f00498f43c10af323d2cdc92
MatchMyTool <Matchmytool@gmail.com>
2026-06-19 10:06:06 +0530

    new commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>hello guys today we are here to make a change</h1>
       <h1>Welcome to My New Website!</h1>
       <p>I just deleted the default Vite template and started my own project.</p>
     </div>
